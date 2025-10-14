ae1c6741f9bc2e50228c7e2735927ddb303ff82a
adrianatello28 <adriana.telloquijanos@mercadolibre.com.co>
2025-10-14 15:34:45 -0500

    Eliminar bajadas en Conta Negocio excepto Point Tap y Tap to Pay no iPhone

--- src/components/ContaNegocioMenu/ContaNegocioMenu.jsx
+++ src/components/ContaNegocioMenu/ContaNegocioMenu.jsx
@@ -44,21 +44,21 @@ const ContaNegocioMenu = () => {
            <div className="conta-negocio-menu__list-section">
              <h3 className="conta-negocio-menu__list-title">Soluções de vendas</h3>
              <ul className="conta-negocio-menu__product-list">
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Maquininhas point</h4><p>Venda mais e pague menos taxas</p></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Maquininhas point</h4></li>
                <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Point Tap</h4><p>Venda usando só o celular</p></li>
                <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Tap to Pay no iPhone</h4><p>Receba por aproximação no iPhone</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Link de pagamento</h4><p>Cobre nas nas redes sociais</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Pix</h4><p>Receba no app ou maquininha</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Checkout para loja virtual</h4><p>Soluções sob medida para sua loja</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Planos de assinatura</h4><p>Faça cobranças automáticas</p></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Link de pagamento</h4></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Pix</h4></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Checkout para loja virtual</h4></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Planos de assinatura</h4></li>
              </ul>
            </div>
            <div className="conta-negocio-menu__list-section">
              <h3 className="conta-negocio-menu__list-title conta-negocio-menu__list-title--disabled">Serviços financeiros</h3>
              <ul className="conta-negocio-menu__product-list">
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Cartão de crédito</h4><p>Peça sem anuidade</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Empréstimos</h4><p>Pague em até 24x</p></li>
-               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Rendimentos e investimentos</h4><p>Rende até 120% do CDI</p></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Cartão de crédito</h4></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Empréstimos</h4></li>
+               <li className="conta-negocio-menu__product-item conta-negocio-menu__product-item--small"><h4>Rendimentos e investimentos</h4></li>
              </ul>
            </div>
            <div className="conta-negocio-menu__list-section">
